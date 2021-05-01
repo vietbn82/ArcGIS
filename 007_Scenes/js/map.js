31b842118884e2b08a97938c765b39be2f364f73
Novi <vietbn@outllook.com>
2021-05-01 15:21:45 +0700

    007

--- 007_Scenes/js/map.js
+++ 007_Scenes/js/map.js
@@ -1,52 +1,32 @@
 require(["esri/config",
     "esri/Map",
-    "esri/views/MapView",
-
-    "esri/Basemap",
-    "esri/layers/VectorTileLayer",
-    "esri/layers/TileLayer",
-
-], function (esriConfig, Map, MapView, Basemap, VectorTileLayer, TileLayer) {
+    "esri/views/SceneView",
+], function (esriConfig, Map, SceneView) {
     ///////////////////////////////////////
     //CONFIG
     ///////////////////////////////////////
     esriConfig.apiKey =
         "AAPK4b64c2dbe7d145f094823d0b55644471j3Jbii5XHNwHd4jCliS78ENvE-1TTC5ZH8v_9RYXlFElVlwD-Jn0Cw8jwBglVozN";
 
-
     ///////////////////////////////////////
     // create map layer
     ///////////////////////////////////////
-
-    const vectorTileLayer = new VectorTileLayer({
-        portalItem: {
-            id: "6976148c11bd497d8624206f9ee03e30" // Forest and Parks Canvas
-        },
-        opacity: .75
+    const map = new Map({
+        basemap: "arcgis-topographic",
+        ground: "world-elevation",
     });
-    const imageTileLayer = new TileLayer({
-        portalItem: {
-            id: "1b243539f4514b6ba35e7d995890db1d" // World Hillshade
+
+    const view = new SceneView({
+        container: "viewDiv",
+        map: map,
+        camera: {
+            position: {
+                x: -118.808, //Longitude
+                y: 33.961, //Latitude
+                z: 2000 //Meters
+            },
+            tilt: 75
         }
     });
 
-    const basemap = new Basemap({
-        baseLayers: [
-
-            imageTileLayer,
-
-        ]
-    });
-    const map = new Map({
-        basemap: basemap,
-    });
-
-    const view = new MapView({
-        container: "viewDiv",
-        map: map,
-    
-        center: [-100,40],
-        zoom: 3
-    
-      });
 });
